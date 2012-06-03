11a43ec5b64c273e7b122f3b02e1aa1e6c0ef392
fpo <Frank.Pohling@t-online.de>
2012-06-03 13:48:42 +0300

    i think that's the correct way.

--- IRremoteInt.h
+++ IRremoteInt.h
@@ -175,7 +175,7 @@ extern volatile irparams_t irparams;
 #define TIMER_ENABLE_PWM     (TCCR2A |= _BV(COM2B1))
 #define TIMER_DISABLE_PWM    (TCCR2A &= ~(_BV(COM2B1)))
 #define TIMER_ENABLE_INTR    (TIMSK2 = _BV(OCIE2A))
-#define TIMER_DISABLE_INTR   (TIMSK2 - 0)
+#define TIMER_DISABLE_INTR   (TIMSK2 = 0)
 #define TIMER_INTR_NAME      TIMER2_COMPA_vect
 #define TIMER_CONFIG_KHZ(val) ({ \
   const uint8_t pwmval = SYSCLOCK / 2000 / (val); \
