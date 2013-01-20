9ba6628f04c21d26181d4211d752482220294782
PaulStoffregen <paul@pjrc.com>
2013-01-20 06:48:13 -0800

    Add support for Teensy 3.0

--- IRremote.cpp
+++ IRremote.cpp
@@ -225,7 +225,7 @@ void IRsend::mark(int time) {
   // Sends an IR mark for the specified number of microseconds.
   // The mark output is modulated at the PWM frequency.
   TIMER_ENABLE_PWM; // Enable pin 3 PWM output
-  delayMicroseconds(time);
+  if (time > 0) delayMicroseconds(time);
 }
 
 /* Leave pin off for time (given in microseconds) */
@@ -233,7 +233,7 @@ void IRsend::space(int time) {
   // Sends an IR space for the specified number of microseconds.
   // A space is no output, so the PWM output is disabled.
   TIMER_DISABLE_PWM; // Disable pin 3 PWM output
-  delayMicroseconds(time);
+  if (time > 0) delayMicroseconds(time);
 }
 
 void IRsend::enableIROut(int khz) {
