e484d67fdf0f5ea22c8e9a976d3ab310e5026405
Luca Giacalone <lucagiacalone@MBP-di-Luca.homenet.telecomitalia.it>
2014-10-14 09:58:19 +0200

    Increased RawBuffer size

--- IRremote.h
+++ IRremote.h
@@ -119,7 +119,7 @@ public:
 // Some useful constants
 
 #define USECPERTICK 50  // microseconds per clock interrupt tick
-#define RAWBUF 100 // Length of raw duration buffer
+#define RAWBUF 400 // Length of raw duration buffer
 
 // Marks tend to be 100us too long, and spaces 100us too short
 // when received due to sensor lag.
