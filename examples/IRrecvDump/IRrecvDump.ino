bd72084b7f2efc893f5364425a8eb110a6dd0e4e
AnalysIR <info@analysir.com>
2015-08-25 00:34:54 +0100

    Bug fixes as per Issue #167
    Have updated IRrecvDdump to fix bugs described in Issue: #167
    
    In summary, removed bug where large space values were displayed incorrectly & confusing users. The output now always starts with a mark, instead of a space, which makes it easier to interpret and less confusing for users.
    
    refer to #167 for more detials.
    
    The update has been tested with several protocols (but not all) and verified as working.

--- examples/IRrecvDump/IRrecvDump.ino
+++ examples/IRrecvDump/IRrecvDump.ino
@@ -10,7 +10,12 @@
 
 #include <IRremote.h>
 
-int RECV_PIN = 11;
+/* 
+*  Default is Arduino pin D11. 
+*  You can change this to another available Arduino Pin.
+*  Your IR receiver should be connected to the pin defined here
+*/
+int RECV_PIN = 11; 
 
 IRrecv irrecv(RECV_PIN);
 
@@ -22,18 +27,17 @@ void setup()
   irrecv.enableIRIn(); // Start the receiver
 }
 
-// Dumps out the decode_results structure.
-// Call this after IRrecv::decode()
-// void * to work around compiler issue
-//void dump(void *v) {
-//  decode_results *results = (decode_results *)v
+
 void dump(decode_results *results) {
+  // Dumps out the decode_results structure.
+  // Call this after IRrecv::decode()
   int count = results->rawlen;
   if (results->decode_type == UNKNOWN) {
     Serial.print("Unknown encoding: ");
   }
   else if (results->decode_type == NEC) {
     Serial.print("Decoded NEC: ");
+
   }
   else if (results->decode_type == SONY) {
     Serial.print("Decoded SONY: ");
@@ -46,21 +50,20 @@ void dump(decode_results *results) {
   }
   else if (results->decode_type == PANASONIC) {
     Serial.print("Decoded PANASONIC - Address: ");
-    Serial.print(results->address,HEX);
+    Serial.print(results->address, HEX);
     Serial.print(" Value: ");
   }
   else if (results->decode_type == LG) {
-     Serial.print("Decoded LG: ");
+    Serial.print("Decoded LG: ");
   }
   else if (results->decode_type == JVC) {
-     Serial.print("Decoded JVC: ");
-
+    Serial.print("Decoded JVC: ");
   }
   else if (results->decode_type == AIWA_RC_T501) {
     Serial.print("Decoded AIWA RC T501: ");
   }
   else if (results->decode_type == WHYNTER) {
-     Serial.print("Decoded Whynter: ");
+    Serial.print("Decoded Whynter: ");
   }
   Serial.print(results->value, HEX);
   Serial.print(" (");
@@ -70,18 +73,18 @@ void dump(decode_results *results) {
   Serial.print(count, DEC);
   Serial.print("): ");
 
-  for (int i = 0; i < count; i++) {
-    if ((i % 2) == 1) {
+  for (int i = 1; i < count; i++) {
+    if (i & 1) {
       Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
     }
     else {
-      Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
+      Serial.write('-');
+      Serial.print((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
     }
     Serial.print(" ");
   }
-  Serial.println("");
+  Serial.println();
 }
-
 
 void loop() {
   if (irrecv.decode(&results)) {
