fdc1c724600d070e8bddbbac9d0936e4e17fc992
DoDi <DrDiettrich1@aol.com>
2016-09-02 17:43:54 +0200

    update example

--- examples/IRrecvNew/IRrecvNew.ino
+++ examples/IRrecvNew/IRrecvNew.ino
@@ -1,9 +1,9 @@
 /*
- * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+ * IRremote: IRrecvNew - demonstrates receiving IR codes with IRrecv
+ * using standard patterns: begin(), available() and read()
+ * 
  * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
+ * Version 2.1 2016-09-02 by DoDi
  */
 
 #include <IRremote.h>
@@ -12,22 +12,14 @@ int RECV_PIN = 11;
 
 IRrecv irrecv(RECV_PIN);
 
-//decode_results results;
-
 void setup()
 {
   Serial.begin(9600);
-  //irrecv.enableIRIn(); // Start the receiver
   irrecv.begin(true); //blinking
 }
 
 void loop() {
-  //if (irrecv.decode(results)) {
-  //if (irrecv.available()) {
   if (irrecv.available(irNEC)) {
-    //if (irrecv.decodeNEC())
       Serial.println(irrecv.read(), HEX);
-    //irrecv.resume(); // Receive the next value
   }
-  delay(100);
 }
