346fc50885648b12043401f0d4af622bdddbfb66
makerMcl <makerMcl@clauss.pro>
2020-03-01 13:35:32 +0100

    fixed compiler warning

--- IRremote.cpp
+++ IRremote.cpp
@@ -20,14 +20,13 @@
 
 // Defining IR_GLOBAL here allows us to declare the instantiation of global variables
 #define IR_GLOBAL
-#	include "IRremote.h"
-#	include "IRremoteInt.h"
+#include "IRremote.h"
+#include "IRremoteInt.h"
 #undef IR_GLOBAL
 
 #ifndef IR_TIMER_USE_ESP32
 #include <avr/interrupt.h>
 #endif
-
 
 //+=============================================================================
 // The match functions were (apparently) originally MACROs to improve code speed
@@ -42,73 +41,71 @@
 //   in a hope of finding out what is going on, but for now they will remain as
 //   functions even in non-DEBUG mode
 //
-int  MATCH (int measured,  int desired)
+int MATCH(int measured, int desired)
 {
- 	DBG_PRINT(F("Testing: "));
- 	DBG_PRINT(TICKS_LOW(desired), DEC);
- 	DBG_PRINT(F(" <= "));
- 	DBG_PRINT(measured, DEC);
- 	DBG_PRINT(F(" <= "));
- 	DBG_PRINT(TICKS_HIGH(desired), DEC);
+    DBG_PRINT(F("Testing: "));
+    DBG_PRINT(TICKS_LOW(desired), DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(measured, DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(TICKS_HIGH(desired), DEC);
 
-  bool passed = ((measured >= TICKS_LOW(desired)) && (measured <= TICKS_HIGH(desired)));
-  if (passed)
-    DBG_PRINTLN(F("?; passed"));
-  else
-    DBG_PRINTLN(F("?; FAILED")); 
- 	return passed;
+    bool passed = ((measured >= TICKS_LOW(desired)) && (measured <= TICKS_HIGH(desired)));
+    if (passed)
+        DBG_PRINTLN(F("?; passed"));
+    else
+        DBG_PRINTLN(F("?; FAILED"));
+    return passed;
 }
 
 //+========================================================
 // Due to sensor lag, when received, Marks tend to be 100us too long
 //
-int  MATCH_MARK (int measured_ticks,  int desired_us)
+int MATCH_MARK(int measured_ticks, int desired_us)
 {
-	DBG_PRINT(F("Testing mark (actual vs desired): "));
-	DBG_PRINT(measured_ticks * USECPERTICK, DEC);
-	DBG_PRINT(F("us vs "));
-	DBG_PRINT(desired_us, DEC);
-	DBG_PRINT("us"); 
-	DBG_PRINT(": ");
-	DBG_PRINT(TICKS_LOW(desired_us + MARK_EXCESS) * USECPERTICK, DEC);
-	DBG_PRINT(F(" <= "));
-	DBG_PRINT(measured_ticks * USECPERTICK, DEC);
-	DBG_PRINT(F(" <= "));
-	DBG_PRINT(TICKS_HIGH(desired_us + MARK_EXCESS) * USECPERTICK, DEC);
+    DBG_PRINT(F("Testing mark (actual vs desired): "));
+    DBG_PRINT(measured_ticks * USECPERTICK, DEC);
+    DBG_PRINT(F("us vs "));
+    DBG_PRINT(desired_us, DEC);
+    DBG_PRINT("us");
+    DBG_PRINT(": ");
+    DBG_PRINT(TICKS_LOW(desired_us + MARK_EXCESS) * USECPERTICK, DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(measured_ticks * USECPERTICK, DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(TICKS_HIGH(desired_us + MARK_EXCESS) * USECPERTICK, DEC);
 
-  bool passed = ((measured_ticks >= TICKS_LOW (desired_us + MARK_EXCESS))
-                && (measured_ticks <= TICKS_HIGH(desired_us + MARK_EXCESS)));
-  if (passed)
-    DBG_PRINTLN(F("?; passed"));
-  else
-    DBG_PRINTLN(F("?; FAILED")); 
- 	return passed;
+    bool passed = ((measured_ticks >= TICKS_LOW(desired_us + MARK_EXCESS)) && (measured_ticks <= TICKS_HIGH(desired_us + MARK_EXCESS)));
+    if (passed)
+        DBG_PRINTLN(F("?; passed"));
+    else
+        DBG_PRINTLN(F("?; FAILED"));
+    return passed;
 }
 
 //+========================================================
 // Due to sensor lag, when received, Spaces tend to be 100us too short
 //
-int  MATCH_SPACE (int measured_ticks,  int desired_us)
+int MATCH_SPACE(int measured_ticks, int desired_us)
 {
-	DBG_PRINT(F("Testing space (actual vs desired): "));
-	DBG_PRINT(measured_ticks * USECPERTICK, DEC);
-	DBG_PRINT(F("us vs "));
-	DBG_PRINT(desired_us, DEC);
-	DBG_PRINT("us"); 
-	DBG_PRINT(": ");
-	DBG_PRINT(TICKS_LOW(desired_us - MARK_EXCESS) * USECPERTICK, DEC);
-	DBG_PRINT(F(" <= "));
-	DBG_PRINT(measured_ticks * USECPERTICK, DEC);
-	DBG_PRINT(F(" <= "));
-	DBG_PRINT(TICKS_HIGH(desired_us - MARK_EXCESS) * USECPERTICK, DEC);
+    DBG_PRINT(F("Testing space (actual vs desired): "));
+    DBG_PRINT(measured_ticks * USECPERTICK, DEC);
+    DBG_PRINT(F("us vs "));
+    DBG_PRINT(desired_us, DEC);
+    DBG_PRINT("us");
+    DBG_PRINT(": ");
+    DBG_PRINT(TICKS_LOW(desired_us - MARK_EXCESS) * USECPERTICK, DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(measured_ticks * USECPERTICK, DEC);
+    DBG_PRINT(F(" <= "));
+    DBG_PRINT(TICKS_HIGH(desired_us - MARK_EXCESS) * USECPERTICK, DEC);
 
-  bool passed = ((measured_ticks >= TICKS_LOW (desired_us - MARK_EXCESS))
-                && (measured_ticks <= TICKS_HIGH(desired_us - MARK_EXCESS)));
-  if (passed)
-    DBG_PRINTLN(F("?; passed"));
-  else
-    DBG_PRINTLN(F("?; FAILED")); 
- 	return passed;
+    bool passed = ((measured_ticks >= TICKS_LOW(desired_us - MARK_EXCESS)) && (measured_ticks <= TICKS_HIGH(desired_us - MARK_EXCESS)));
+    if (passed)
+        DBG_PRINTLN(F("?; passed"));
+    else
+        DBG_PRINTLN(F("?; FAILED"));
+    return passed;
 }
 
 //+=============================================================================
@@ -126,75 +123,92 @@ int  MATCH_SPACE (int measured_ticks,  int desired_us)
 #ifdef IR_TIMER_USE_ESP32
 void IRTimer()
 #else
-ISR (TIMER_INTR_NAME)
+ISR(TIMER_INTR_NAME)
 #endif
 {
-	TIMER_RESET;
+    TIMER_RESET;
 
-	// Read if IR Receiver -> SPACE [xmt LED off] or a MARK [xmt LED on]
-	// digitalRead() is very slow. Optimisation is possible, but makes the code unportable
-	uint8_t  irdata = (uint8_t)digitalRead(irparams.recvpin);
+    // Read if IR Receiver -> SPACE [xmt LED off] or a MARK [xmt LED on]
+    // digitalRead() is very slow. Optimisation is possible, but makes the code unportable
+    uint8_t irdata = (uint8_t)digitalRead(irparams.recvpin);
 
-	irparams.timer++;  // One more 50uS tick
-	if (irparams.rawlen >= RAWBUF)  irparams.rcvstate = STATE_OVERFLOW ;  // Buffer overflow
+    irparams.timer++; // One more 50uS tick
+    if (irparams.rawlen >= RAWBUF)
+        irparams.rcvstate = STATE_OVERFLOW; // Buffer overflow
 
-	switch(irparams.rcvstate) {
-		//......................................................................
-		case STATE_IDLE: // In the middle of a gap
-			if (irdata == MARK) {
-				if (irparams.timer < GAP_TICKS)  {  // Not big enough to be a gap.
-					irparams.timer = 0;
+    switch (irparams.rcvstate)
+    {
+    //......................................................................
+    case STATE_IDLE: // In the middle of a gap
+        if (irdata == MARK)
+        {
+            if (irparams.timer < GAP_TICKS)
+            { // Not big enough to be a gap.
+                irparams.timer = 0;
+            }
+            else
+            {
+                // Gap just ended; Record duration; Start recording transmission
+                irparams.overflow = false;
+                irparams.rawlen = 0;
+                irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+                irparams.timer = 0;
+                irparams.rcvstate = STATE_MARK;
+            }
+        }
+        break;
+    //......................................................................
+    case STATE_MARK: // Timing Mark
+        if (irdata == SPACE)
+        { // Mark ended; Record time
+            irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+            irparams.timer = 0;
+            irparams.rcvstate = STATE_SPACE;
+        }
+        break;
+    //......................................................................
+    case STATE_SPACE: // Timing Space
+        if (irdata == MARK)
+        { // Space just ended; Record time
+            irparams.rawbuf[irparams.rawlen++] = irparams.timer;
+            irparams.timer = 0;
+            irparams.rcvstate = STATE_MARK;
+        }
+        else if (irparams.timer > GAP_TICKS)
+        { // Space
+            // A long Space, indicates gap between codes
+            // Flag the current code as ready for processing
+            // Switch to STOP
+            // Don't reset timer; keep counting Space width
+            irparams.rcvstate = STATE_STOP;
+        }
+        break;
+    //......................................................................
+    case STATE_STOP: // Waiting; Measuring Gap
+        if (irdata == MARK)
+            irparams.timer = 0; // Reset gap timer
+        break;
+    //......................................................................
+    case STATE_OVERFLOW: // Flag up a read overflow; Stop the State Machine
+        irparams.overflow = true;
+        irparams.rcvstate = STATE_STOP;
+        break;
+    }
 
-				} else {
-					// Gap just ended; Record duration; Start recording transmission
-					irparams.overflow                  = false;
-					irparams.rawlen                    = 0;
-					irparams.rawbuf[irparams.rawlen++] = irparams.timer;
-					irparams.timer                     = 0;
-					irparams.rcvstate                  = STATE_MARK;
-				}
-			}
-			break;
-		//......................................................................
-		case STATE_MARK:  // Timing Mark
-			if (irdata == SPACE) {   // Mark ended; Record time
-				irparams.rawbuf[irparams.rawlen++] = irparams.timer;
-				irparams.timer                     = 0;
-				irparams.rcvstate                  = STATE_SPACE;
-			}
-			break;
-		//......................................................................
-		case STATE_SPACE:  // Timing Space
-			if (irdata == MARK) {  // Space just ended; Record time
-				irparams.rawbuf[irparams.rawlen++] = irparams.timer;
-				irparams.timer                     = 0;
-				irparams.rcvstate                  = STATE_MARK;
-
-			} else if (irparams.timer > GAP_TICKS) {  // Space
-					// A long Space, indicates gap between codes
-					// Flag the current code as ready for processing
-					// Switch to STOP
-					// Don't reset timer; keep counting Space width
-					irparams.rcvstate = STATE_STOP;
-			}
-			break;
-		//......................................................................
-		case STATE_STOP:  // Waiting; Measuring Gap
-		 	if (irdata == MARK)  irparams.timer = 0 ;  // Reset gap timer
-		 	break;
-		//......................................................................
-		case STATE_OVERFLOW:  // Flag up a read overflow; Stop the State Machine
-			irparams.overflow = true;
-			irparams.rcvstate = STATE_STOP;
-		 	break;
-	}
-
-	// If requested, flash LED while receiving IR data
-	if (irparams.blinkflag) {
-		if (irdata == MARK)
-			if (irparams.blinkpin) digitalWrite(irparams.blinkpin, HIGH); // Turn user defined pin LED on
-				else BLINKLED_ON() ;   // if no user defined LED pin, turn default LED pin for the hardware on
-		else if (irparams.blinkpin) digitalWrite(irparams.blinkpin, LOW); // Turn user defined pin LED on
-				else BLINKLED_OFF() ;   // if no user defined LED pin, turn default LED pin for the hardware on
-	}
+    // If requested, flash LED while receiving IR data
+    if (irparams.blinkflag)
+    {
+        if (irdata == MARK)
+        {
+            if (irparams.blinkpin)
+                digitalWrite(irparams.blinkpin, HIGH); // Turn user defined pin LED on
+                                                       //else BLINKLED_ON();   // if no user defined LED pin, turn default LED pin for the hardware on
+        }
+        else
+        {
+            if (irparams.blinkpin)
+                digitalWrite(irparams.blinkpin, LOW); // Turn user defined pin LED on
+                                                      //else BLINKLED_OFF();   // if no user defined LED pin, turn default LED pin for the hardware on
+        }
+    }
 }
