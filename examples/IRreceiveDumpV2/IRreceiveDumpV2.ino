690c472e6edfac1b6ad0d9ea77715b26a9145035
Iosif Peterfi <peterfi@mpdl.mpg.de>
2020-12-06 10:28:45 +0100

    Added test call for dumpPronto(*String) to IRreceiveDumpV2

--- examples/IRreceiveDumpV2/IRreceiveDumpV2.ino
+++ examples/IRreceiveDumpV2/IRreceiveDumpV2.ino
@@ -71,6 +71,8 @@ void loop() {
             Serial.println();                               // blank line between entries
         }
 
+        delete ProntoHEX;                               // Delete string object
+
         IrReceiver.resume();                            // Prepare for the next value
     }
 }
