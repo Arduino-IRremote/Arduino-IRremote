d2d9523dcef539e11687fcbbb76175d4155a5e27
Armin <armin.joachimsmeyer@gmail.com>
2022-01-28 02:21:49 +0100

    RP2040 support

--- examples/IRDispatcherDemo/IRDispatcherDemo.ino
+++ examples/IRDispatcherDemo/IRDispatcherDemo.ino
@@ -113,7 +113,7 @@ void doTone2200();
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(115200);
-#if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL) || defined(ARDUINO_attiny3217)
+#if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) || defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
     delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
 #endif
 #if defined(ESP8266)
